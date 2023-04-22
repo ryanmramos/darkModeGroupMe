11a78659a911670dc71218db286f09ac1400890f
ryanmramos <ryan.ramosenhs@gmail.com>
2023-04-21 19:02:54 -0500

    pillar element

--- content.js
+++ content.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', function() {
+
+    // handle body
     const body = document.querySelector('body');
     if (body) {
         console.log(body);
@@ -7,4 +9,32 @@ window.addEventListener('load', function() {
     else {
         console.log("Body element does not exist.");
     }
+
+    // handle app
+    const appDiv = body.querySelector('#app')
+    console.log(appDiv);
+    if (appDiv) {
+        appDiv.style.backgroundColor = '#92959B';
+    }
+    else {
+        console.log('Could not get appDiv');
+    }
+    
+    // handle pillar element
+    handlePillar();
 });
+
+function handlePillar() {
+    console.log("In handlePillar");
+    const appDiv = body.querySelector('body #app');
+
+    // interval to wait for pillar element
+    const interval = setInterval(() => {
+        const pillar = appDiv.querySelector('aside .pillar');
+        if (pillar) {
+            clearInterval(interval);
+            console.log(pillar);
+            pillar.style.backgroundColor = '#202226';
+        }
+    }, 50);
+}
